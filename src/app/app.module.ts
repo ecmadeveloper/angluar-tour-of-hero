75e03c6e0c86cd317f8a2a097b78ebcf5c00da54
ecmadeveloper <ecma6.ninja@gmail.com>
2017-02-22 02:22:11 -0500

    hero update was done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } 		from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent }				from './app.component';
 import { HeroesComponent }			from './heroes.component';
 import { HeroDetailComponent }		from './hero-detail.component';
 import { HeroService } 				from './hero.service';
 import { DashboardComponent }		from './dashboard.component';
-import { AppRoutingModule } 		from './app-routing.module';
 
 @NgModule({
 	declarations: [
@@ -21,7 +26,8 @@ import { AppRoutingModule } 		from './app-routing.module';
 		BrowserModule,
 		FormsModule,
 		AppRoutingModule,
-		HttpModule
+		HttpModule,
+		InMemoryWebApiModule.forRoot(InMemoryDataService)
 	],
 	providers: [HeroService],
 	bootstrap: [AppComponent]
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,8 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
-
-import 'rxjs/add/operator/switchMap';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -10,11 +9,12 @@ import { HeroService } from './hero.service';
 @Component({
 	moduleId: module.id,
 	selector: 'my-hero-detail',
-	templateUrl: './hero-detail.component.html'
+	templateUrl: './hero-detail.component.html',
+	styleUrls: ['./hero-detail.component.css']
 })
 
 export class HeroDetailComponent implements OnInit {
-	@Input() hero: Hero;
+	hero: Hero;
 
 	constructor(
 		private heroService: HeroService,
@@ -30,5 +30,10 @@ export class HeroDetailComponent implements OnInit {
 
 	goBack(): void {
 		this.location.back();
+	}
+
+	save(): void {
+		this.heroService.update(this.hero)
+			.then(() => this.goBack());
 	}
 }
