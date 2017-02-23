8524c0bc42639fb50a1dde11531332952605f4b0
ecmadeveloper <ecma6.ninja@gmail.com>
2017-02-23 07:27:51 -0500

    http implemented

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -4,10 +4,10 @@ import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
+	moduleId: module.id,
 	selector: 'my-heroes',
 	templateUrl: './heroes.component.html',
-	styleUrls: ['./heroes.component.css'],
-	providers: [HeroService]
+	styleUrls: ['./heroes.component.css']
 })
 
 export class HeroesComponent implements OnInit {
@@ -25,6 +25,26 @@ export class HeroesComponent implements OnInit {
 
 	getHeroes(): void {
 		this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+	}
+
+	add(name:string): void {
+		name = name.trim();
+		if (!name) { return; }
+
+		this.heroService.create(name)
+			.then(hero => {
+				this.heroes.push(hero);
+				this.selectedHero = null;
+			});
+	}
+
+	delete(hero: Hero): void {
+		this.heroService
+			.delete(hero.id)
+			.then(() => {
+				this.heroes = this.heroes.filter(h => h !== hero);
+				if (this.selectedHero === hero) { this.selectedHero = null; }
+			})
 	}
 
 	gotoDetail(): void {
